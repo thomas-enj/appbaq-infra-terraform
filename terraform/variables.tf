7da018ae2a55f5ed9bcb18e66cf8b3e743e1a23a
Thomas Enjalbert <thomas.enjalbert.pro@gmail.com>
2026-07-31 14:26:44 +0200

    feat: remove arguments from terraform_tflint hook and comment out unused variables in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,17 +13,17 @@ variable "resource_group_name" {
   type        = string
 }
 
-variable "location" {
-  description = "Azure region for resources"
-  type        = string
-  default     = "francecentral"
-}
+# variable "location" {
+#   description = "Azure region for resources"
+#   type        = string
+#   default     = "francecentral"
+# }
 
-variable "shared_rg_name" {
-  description = "Resource Group containing the shared App Service plan"
-  type        = string
-  default     = "rg-shared-prf2026"
-}
+# variable "shared_rg_name" {
+#   description = "Resource Group containing the shared App Service plan"
+#   type        = string
+#   default     = "rg-shared-prf2026"
+# }
 
 variable "tags" {
   description = "Additional tags to merge with default tags"
